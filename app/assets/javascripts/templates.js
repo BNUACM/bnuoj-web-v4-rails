cdfc1ee04d8b15f76202b05b82d7a896d4a280f7
Chen Ran <crccw@moonux.org>
2015-03-10 14:55:39 +0800

    mistype

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -6,6 +6,6 @@ Handlebars.registerHelper('basePath', function() {
   return basePath;
 });
 
-Handlerbars.registerHelper('t', function(key, options) {
+Handlebars.registerHelper('t', function(key, options) {
   return I18n.t(key, options.hash);
 });
