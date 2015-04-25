d7b1f845b7560ffb8e070aa51dd334955d87d189
Chen Ran <crccw@moonux.org>
2015-04-25 23:26:47 +0800

    add prefix for custom event

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,7 +1,7 @@
 (function($) {
   BNUOJ.Events = {
     // AJAX form submission complete successfully
-    // @param responseJSON    JSON returned by server
-    AJAX_FORM_CORRECT: "ajax_form_correct"
+    // @param responseJSON JSON returned by server
+    AJAX_FORM_CORRECT: "bnuoj-ajax-form-correct"
   }
 }).call(this, jQuery);
